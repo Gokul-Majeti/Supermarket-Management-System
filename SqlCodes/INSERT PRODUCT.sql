a8f35e9b0299cbd75f596b4b444c785a38080de3
Ganesh Pavan Gokul Majeti <ganeshsivamajeti572@gmail.com>
2025-11-22 18:20:18 +0530

    Create INSERT PRODUCT.sql

---
+++ SqlCodes/INSERT PRODUCT.sql
@@ -0,0 +1,13 @@
+INSERT INTO Product VALUES
+(1, 'Amul Gold Milk', 2, 62.00, 150, 'litre', 'BAR001', '2025-11-25', '2025-11-10', 'Full cream milk', 'Amul'),
+(2, 'Britannia Bread', 7, 45.00, 200, 'piece', 'BAR002', '2025-11-20', '2025-11-15', 'Whole wheat bread 400g', 'Britannia'),
+(3, 'Parle-G Biscuits', 4, 20.00, 500, 'piece', 'BAR003', '2026-06-30', '2025-10-01', 'Glucose biscuits 200g', 'Parle'),
+(4, 'Tata Tea Gold', 3, 245.00, 80, 'kg', 'BAR004', '2026-10-30', '2025-05-15', 'Premium tea leaves 500g', 'Tata'),
+(5, 'Coca Cola', 3, 90.00, 300, 'litre', 'BAR005', '2026-03-30', '2025-09-01', 'Carbonated soft drink 2L', 'Coca-Cola'),
+(6, 'Lays Chips', 4, 40.00, 400, 'piece', 'BAR006', '2026-02-28', '2025-10-10', 'Potato chips 100g', 'Lays'),
+(7, 'Maggi Noodles', 4, 14.00, 600, 'piece', 'BAR007', '2026-08-31', '2025-06-20', '2-minute noodles 70g', 'Nestle'),
+(8, 'Dove Soap', 5, 65.00, 120, 'piece', 'BAR008', '2027-12-31', '2024-12-01', 'Moisturizing soap 100g', 'Dove'),
+(9, 'Surf Excel', 6, 180.00, 90, 'kg', 'BAR009', '2028-01-31', '2024-11-01', 'Detergent powder 1kg', 'Surf Excel'),
+(10, 'Basmati Rice', 1, 450.00, 70, 'kg', 'BAR010', '2026-11-30', '2025-05-01', 'Premium long grain rice 5kg', 'India Gate'),
+(11, 'Fresh Milk', 2, 32.00, 250, 'litre', 'BAR011', '2025-11-22', '2025-11-18', 'Toned milk 500ml', 'Mother Dairy'),
+(12, 'Corn Flakes', 11, 220.00, 110, 'kg', 'BAR012', '2026-07-31', '2025-08-01', 'Breakfast cereal 500g', 'Kelloggs');
