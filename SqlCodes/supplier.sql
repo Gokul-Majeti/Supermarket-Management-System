7c16f9a4f56051f059eaecc78d22855891f5bc71
Ganesh Pavan Gokul Majeti <ganeshsivamajeti572@gmail.com>
2025-11-22 18:05:00 +0530

    Create Supplier table with relevant fields

---
+++ SqlCodes/supplier.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Supplier (
+    Supplier_ID INT PRIMARY KEY,
+    Supplier_Name VARCHAR(100) NOT NULL,
+    Phone_Number VARCHAR(15) NOT NULL,
+    Email VARCHAR(100),
+    Street VARCHAR(100),
+    Area VARCHAR(100),
+    Landmark VARCHAR(100),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Postal_Code VARCHAR(10),
+    Payment_Terms VARCHAR(100),
+    Tax_ID VARCHAR(50)
+);
