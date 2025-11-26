d659d70d27edfc2a4da0fa456dbbade33c0da539
Ganesh Pavan Gokul Majeti <ganeshsivamajeti572@gmail.com>
2025-11-26 08:57:02 +0530

    Update and rename Query1 to Query1.sql

--- SqlCodes/Query1.sql
+++ SqlCodes/Query1.sql
@@ -1,4 +1,4 @@
-Query : Update Subtotal in Bill_Item (Derived Attribute)
+--Query : Update Subtotal in Bill_Item (Derived Attribute)
 SET sql_safe_updates =0;
 UPDATE Bill_Item
 SET Subtotal = (Quantity * Unit_Price) - Discount;
